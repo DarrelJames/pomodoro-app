68f0cd45d571f1dcd5aaf13e66b26b333f3a0d47
Darrel Castellano <dcastellano2007@gmail.com>
2019-12-04 17:59:17 -0800

    Create Seeds file

--- pomodoro-api/db/seeds.rb
+++ pomodoro-api/db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  s = Session.create(date: Faker::Date.between(from: 2.days.ago, to: Date.today), start_time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now) , end_time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now))
+
+  5.times do
+    s.notes.create(content: Faker::Lorem.sentence(word_count: 10))
+  end
+
+end
+
+puts "Created #{Session.count} sessions"
+puts "Created #{Note.count} notes"
+# create_table "notes", force: :cascade do |t|
+#   t.string "content"
+#   t.bigint "session_id", null: false
+#   t.datetime "created_at", precision: 6, null: false
+#   t.datetime "updated_at", precision: 6, null: false
+#   t.index ["session_id"], name: "index_notes_on_session_id"
+# end
+#
+# create_table "sessions", force: :cascade do |t|
+#   t.datetime "date"
+#   t.datetime "start_time"
+#   t.datetime "end_time"
+#   t.datetime "created_at", precision: 6, null: false
+#   t.datetime "updated_at", precision: 6, null: false
