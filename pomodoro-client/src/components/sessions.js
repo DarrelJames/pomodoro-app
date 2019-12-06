9449e2100f27ca64e4bb945034365e45c1e0d8fe
Darrel Castellano <dcastellano2007@gmail.com>
2019-12-05 17:18:25 -0800

    Add function to fetch and load sessions in sessions.js

--- pomodoro-client/src/components/sessions.js
+++ pomodoro-client/src/components/sessions.js
@@ -3,12 +3,25 @@ class Sessions {
     this.sessions = []
     this.initBindingsAndEventListeners()
     this.adapter = new SessionsAdapter()
+    this.fetchAndLoadNote()
   }
 
   initBindingsAndEventListeners() {
-
+    this.sessionForm = document.getElementById('new-session')
+    this.sessionContainer = document.getElementById('session-container')
   }
 
+  fetchAndLoadNote() {
+    this.adapter.getSessions()
+      .then(sessions => {
+        return sessions.data.forEach(session => this.sessions.push(session))
+      })
+      .then(() => {
+        this.render()
+      })
+  }
 
-
+  render() {
+    this.sessionContainer.innerHTML = 'sessions going here'
+  }
 }
