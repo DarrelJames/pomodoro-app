0df8a071e19ee97f809f8b7726357671773c56c7
Darrel Castellano <dcastellano2007@gmail.com>
2019-12-09 16:32:40 -0800

    Add renderLi function and set attributes to new Date

--- pomodoro-client/src/components/session.js
+++ pomodoro-client/src/components/session.js
@@ -1,9 +1,20 @@
 class Session {
   constructor(sessionJSON) {
     this.id = sessionJSON.id
-    this.date = sessionJSON.attributes.date
-    this.start_time = sessionJSON.attributes.start_time
-    this.end_time = sessionJSON.attributes.end_time
+    this.date = new Date(sessionJSON.attributes.date)
+    this.start_time = new Date(sessionJSON.attributes.start_time)
+    this.end_time = new Date(sessionJSON.attributes.end_time)
     this.notes = sessionJSON.relationships.notes.data
+  }
+
+  renderLi() {
+    return `
+    <li>
+      <div class="card">
+      ${this.start_time.toLocaleTimeString()}
+      ${this.end_time.toLocaleTimeString()}
+      ${this.date.toDateString()}
+      </div>
+    </li>`
   }
 }
