77ee561d869391b18d90d25851dfd976e8d89dcc
Darrel Castellano <dcastellano2007@gmail.com>
2019-12-12 17:23:59 -0800

    add getter for start time

--- pomodoro-client/src/components/session.js
+++ pomodoro-client/src/components/session.js
@@ -2,16 +2,18 @@ class Session {
   constructor(sessionJSON) {
     this.id = sessionJSON.id
     this.date = new Date(sessionJSON.attributes.date)
-    this.start_time = new Date(sessionJSON.attributes.start_time)
     this.end_time = new Date(sessionJSON.attributes.end_time)
     this.notes = sessionJSON.relationships.notes.data
   }
 
+  get start_time() {
+    return this.date.toLocaleTimeString()
+  }
   renderLi() {
     return `
     <li>
       <div class="card">
-      <p>${this.start_time.toLocaleTimeString()}</p>
+      <p>${this.start_time}</p>
       <p>${this.end_time.toLocaleTimeString()}</p>
       <p>${this.date.toDateString()}</p>
       </div>
