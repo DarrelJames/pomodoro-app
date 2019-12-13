7306489b8befe266ab5ec436b60cdc19bd1bacb8
Darrel Castellano <dcastellano2007@gmail.com>
2019-12-13 12:08:27 -0800

    Set end_time to null on new session.

--- pomodoro-client/src/components/session.js
+++ pomodoro-client/src/components/session.js
@@ -2,7 +2,7 @@ class Session {
   constructor(sessionJSON) {
     this.id = sessionJSON.id
     this.date = new Date(sessionJSON.attributes.date)
-    this.end_time = new Date(sessionJSON.attributes.end_time)
+    this.end_time = sessionJSON.attributes.end_time ? new Date(sessionJSON.attributes.end_time) : null
     this.notes = sessionJSON.relationships.notes.data
   }
 
@@ -11,12 +11,10 @@ class Session {
   }
   renderLi() {
     return `
-    <li>
-      <div class="card">
-      <p>${this.start_time}</p>
-      <p>${this.end_time.toLocaleTimeString()}</p>
-      <p>${this.date.toDateString()}</p>
+      <div class="card" data-id="${this.id}">
+      <div class="card-info"><p>${this.start_time}</p><p>${this.end_time ? this.end_time.toLocaleTimeString():"Not Stopped Yet"}</p><p>${this.date.toDateString()}</p></div>
+      <div><button>Add Note</button></div>
       </div>
-    </li>`
+    `
   }
 }
