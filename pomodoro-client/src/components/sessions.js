bb16483841ba26305414646238bf2f0f65c3076a
Darrel Castellano <dcastellano2007@gmail.com>
2019-12-12 20:12:56 -0800

    Add createSession and Updatesession function to create and update

--- pomodoro-client/src/components/sessions.js
+++ pomodoro-client/src/components/sessions.js
@@ -11,6 +11,21 @@ class Sessions {
     this.sessionContainer = document.getElementById('session-container')
   }
 
+  createSession() {
+    const value = new Date()
+
+    return this.adapter.createSession(value).then(session => {
+      const newSession = new Session(session.data)
+
+      this.sessions.push(newSession)
+      this.render()
+    })
+  }
+
+  updateSession(obj) {
+    this.adapter.updateSession(obj)
+  }
+
   fetchAndLoadSession() {
     this.adapter.getSessions()
       .then(sessions => {
@@ -25,5 +40,5 @@ class Sessions {
     const sessionString = this.sessions.map(session => session.renderLi())
 
     this.sessionContainer.innerHTML = sessionString.join("")
-  }  
+  }
 }
