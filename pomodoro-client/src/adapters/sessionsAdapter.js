5efec8694aeab4357346bb2b7a56594281020f42
Darrel Castellano <dcastellano2007@gmail.com>
2019-12-12 20:12:26 -0800

    Add updateSession function to u pdate a session object

--- pomodoro-client/src/adapters/sessionsAdapter.js
+++ pomodoro-client/src/adapters/sessionsAdapter.js
@@ -8,15 +8,29 @@ class SessionsAdapter {
   }
 
   createSession(body) {
+    const session = {
+      date: body
+    }
     const sessionCreateParams = {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
       },
-      body: JSON.stringify({
-        body
-      })
+      body: JSON.stringify({session})
     }
     return fetch(this.baseUrl, sessionCreateParams).then(resp => resp.json())
+  }
+
+  updateSession(session) {
+    const sessionUpdateParams = {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({session})
+    }
+    return fetch(`${this.baseUrl}/${session.id}`, sessionUpdateParams).then(resp => resp.json())
   }
 }
