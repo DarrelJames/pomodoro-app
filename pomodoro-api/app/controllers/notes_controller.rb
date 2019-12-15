799779d34c7ffdbcf95a7c77a19e3f2a553dd7bc
Darrel Castellano <dcastellano2007@gmail.com>
2019-12-15 11:22:50 -0800

    Create and persist new note to datebase

--- pomodoro-api/app/controllers/notes_controller.rb
+++ pomodoro-api/app/controllers/notes_controller.rb
@@ -1,2 +1,18 @@
 class NotesController < ApplicationController
+  def create
+    binding.pry
+    session = Session.find_by_id(params[:session_id])
+
+    note = session.notes.build(note_params)
+
+    if note.save
+      render json: NoteSerializer.new(note)
+    else
+      render json: "Something went wrong"
+    end
+  end
+
+  def note_params
+    params.require(:note).permit(:content)
+  end
 end
