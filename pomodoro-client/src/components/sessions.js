ac0b581b288be0a8564f74a80d735932b6ef3342
Darrel Castellano <dcastellano2007@gmail.com>
2019-12-15 10:23:26 -0800

    Add event delegation for note button and form

--- pomodoro-client/src/components/sessions.js
+++ pomodoro-client/src/components/sessions.js
@@ -10,11 +10,21 @@ class Sessions {
     this.sessionForm = document.getElementById('new-session')
     this.sessionContainer = document.getElementById('session-container')
     this.sessionContainer.addEventListener('click', this.handleClick.bind(this))
+
   }
 
   handleClick(e) {
     e.preventDefault()
+    // debugger
+    if (e.target.className === "open-button") {
+      e.target.style.display = "none"
+      e.target.nextElementSibling.style.display = "block"
+    }
 
+    if (e.target.className === "btn cancel") {
+      e.target.parentElement.parentElement.previousElementSibling.style.display = "block"
+      e.target.parentElement.parentElement.style.display = "none"
+    }
   }
   createSession() {
     const value = new Date()
