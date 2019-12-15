d89feb09a64464209bc49701b2b906a767b34842
Darrel Castellano <dcastellano2007@gmail.com>
2019-12-15 13:50:37 -0800

    Use instance variable to set sessionId

--- pomodoro-client/src/components/notes.js
+++ pomodoro-client/src/components/notes.js
@@ -17,11 +17,12 @@ class Notes {
 
     if (e.target.className === "btn") {
       const noteContent = e.target.form.elements[0].value
-      const sessionId = e.target.parentElement.parentElement.parentElement.dataset.id
+      this.sessionId = e.target.parentElement.parentElement.parentElement.dataset.id
 
-      this.adapter.createNote({sessionId, noteContent}).then((note) => {        
-        const session = note.data.relationships.session.data.id
-        app.sessions.sessions.find(e => e.id === session).notes.push(new Note(note.data))
+      this.adapter.createNote({sessionId: this.sessionId, noteContent }).then((note) => {
+        app.sessions.sessions.find(e => {
+          return e.id === parseInt(this.sessionId)
+        }).notes.push(new Note(note))
         app.sessions.render()
       })
     }
